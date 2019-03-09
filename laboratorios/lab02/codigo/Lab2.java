8e649af2fc7ce76049b25ac695597decdf2ccde1
Alismos <46933082+Alismos@users.noreply.github.com>
2019-03-08 20:59:10 -0500

    Update Lab2.java

--- laboratorios/lab02/codigo/Lab2.java
+++ laboratorios/lab02/codigo/Lab2.java
@@ -1,213 +1,139 @@
-
-/**
- * Write a description of class Lab2 here.
- *
- * @author (your name)
- * @version (a version number or a date)
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
-public class Lab2
+package Laboratorio_2;
+
+import java.util.Random;
+import java.util.Arrays;
+
+public class Code_source
 {
-    public int countEvens(int[] nums) {
-  int acum=0;
-  for(int i=0; i<nums.length;i++){
-    if((nums[i]%2)==0){
-      acum++;
-    }
-  }
-  return acum;
-}
-
-public int bigDiff(int[] nums) {
-  int min=nums[0];
-  int max=nums[0];
-  for(int i=0;i<nums.length;i++){
-    if(nums[i]<min){
-      min=nums[i];
-    }
-    if(nums[i]>max){
-      max=nums[i];
-    }
-  }
-  return max-min;
-}
-
-public int centeredAverage(int[] nums) {
-  int min=nums[0];
-  int max=nums[0];
-  int acum=0;
-  for(int i=0;i<nums.length;i++){
-    if(nums[i]<min){
-      min=nums[i];
-    }
-    if(nums[i]>max){
-      max=nums[i];
-    }
-    acum+=nums[i];
+  // Computer the sum of an array
+  public static int ArraySum(int[] A)
+  {          
+      int sum = 0;
+      for (int i = 0; i < A.length; i++)
+         sum = sum + A[i];
+      return sum;
   }
   
-  return (acum-max-min)/(nums.length-2);
-}
-
-public int sum13(int[] nums) {
-  if(nums.length==0){
-    return 0;
+  // Computes the maximum value of an array
+  public static int ArrayMax(int[] A)
+  {
+      int max = A[0];
+      for (int i = 0; i < A.length; i++)
+         if (A[i] > max)
+           max = A[i];
+      return max;
   }
-  int acum=0;
-  for(int i=0;i<nums.length;i++){
-    if(nums[i]!=13){
-      acum+=nums[i];
-    }else if(nums[i] == 13 && i < nums.length -1 ) {
-  nums[i]=0;
-  nums[i+1] =0;
-    }
-  }
-  return acum;
-}
-
-public int sum67(int[] nums) {
-   int acum=0;
-   int nulo=0;
-   for(int i=0;i<nums.length;i++){
-     if(nums[i]==6){
-       for(int j=i;nums[j]!=7;j++){
-        nums[j]=0;
-        nulo=j;
+  
+  // Sorts an array using Insertion Sort
+  public static void InsertionSort(int[] A)
+  {
+       int temp,j;
+       for (int i = 0; i < A.length; i++)
+       {
+         j = i;
+         while (j > 0 && A[j-1] > A[j])
+           {
+               temp = A[j];
+               A[j] = A[j-1];
+               A[j-1] = temp;
+               j = j-1;
+            }           
        }
-       nums[nulo+1]=0;
-     }else{
-       acum+=nums[i];
-     }
-     
-   }
-   return acum;
-}
-
-public boolean has22(int[] nums) {
- if(nums.length>=2){
-   for(int i=0;i<nums.length-1;i++){
-     if(nums[i]==2 && nums[i+1]==2){
-       return true;
-     }
-     
-   }
- }
- 
- return false;
-}
-
-public boolean lucky13(int[] nums) {
-  
-  boolean h=true;
-  for(int i=0; i<nums.length;i++){
-    if(nums[i]==1 || nums[i]==3){
-      h=false;
-    }
-  }
-  return h;
-}
-
-public boolean sum28(int[] nums) {
-  int acum =0;
-  for(int i:nums){
-    if(i==2){
-      acum+=i;
-    }
-  }if(acum>8){
-      return false;
-  }
-  return acum==8;
+      // System.out.println(Arrays.toString(A));
   }
   
-
-  public boolean more14(int[] nums) {
-  int acum1=0;
-  int acum4=0;
-  boolean xd=true;
-  for(int i=0;i<nums.length;i++){
-    if(nums[i]==1){
-      acum1++;
-    }
-    if(nums[i]==4){
-      acum4++;
-    }
-  }
-  if(acum1>acum4){
-    return true;
-  }else;
-  return false;
-}
-
-public int[] fizzArray(int n) {
-  int [] a= new int[n];
-  for(int b=0; b < n; b++){
-   a[b]=b; 
-  }
-  return a;
-  }
-
-  public boolean isEverywhere(int[] nums, int val) {
-  for(int a=0; a < nums.length-1;a++){
-    
-    if(!((nums[a]==val)||(nums[a+1]==val))){
-      return false;
-    }
-    
-  }
-  return true;
-}
-
-public String[] fizzBuzz(int start, int end) {
-  String[] a= new String [end-start];
-  for(int b=start; b<end;b++){
-    if(b%3==0 && b%5==0){
-      a[b-start]="FizzBuzz";
-    }else if(b%3==0){
-      a[b-start]="Fizz";
-    }else if(b%5==0){
-      a[b-start]="Buzz";
-    }else{
-      a[b-start]=(new Integer(b)).toString();
-    }
-  }
-  return a;
-}
-
-
-public int maxSpan(int[] nums) {
-  if(nums.length>0){
- int sum=1;
- for (int left=0;left<nums.length;left++){
-   for(int rigth=nums.length-1;rigth>left;rigth--){
-     if(nums[left]==nums[rigth]){
-       int help=rigth-left+1;
-       if(sum<help){
-       sum=help;
-       break;
-       }
-     }
-   }
- }
-  return sum;
-  }else
-  return 0;
-}
-
-  public int[] fix34(int[] nums) {
-    
-    for(int i=0;i<nums.length;i++){
-    if(nums[i]==3){
-      int var=nums[i+1];
-      nums[i+1]=4;
-      for(int j=i+2;j<nums.length;j++){
-        if(nums[j]==4){
-          if(nums[j]==4)
-          nums[j]=var;
+  public static int[] CrearArray(){
+      
+      Random generator = new Random();
+      int num = (int) (Math.random() * 1200000) + 400000;
+      
+      int[] array = new int[num];
+        
+        for (int i =0; i<num; i++){
+            array[i] = generator.nextInt(200000);
         }
-      }
-    }  
+        return array;
     }
-    return nums;
-}
+  
+
+      // Sorts an array using Merge Sort
+      // Taken from www.cs.cmu.edu/
+	public static void mergeSort(int [ ] a)
+	{
+		int[] tmp = new int[a.length];
+		mergeSort(a, tmp,  0,  a.length - 1);
+	}
 
 
+	private static void mergeSort(int [ ] a, int [ ] tmp, int left, int right)
+	{
+		if( left < right )
+		{
+			int center = (left + right) / 2;
+			mergeSort(a, tmp, left, center);
+			mergeSort(a, tmp, center + 1, right);
+			merge(a, tmp, left, center + 1, right);
+		}
+	}
+
+
+    private static void merge(int[ ] a, int[ ] tmp, int left, int right, int rightEnd )
+    {
+        int leftEnd = right - 1;
+        int k = left;
+        int num = rightEnd - left + 1;
+
+        while(left <= leftEnd && right <= rightEnd)
+            if(a[left] <= a[right] )
+                tmp[k++] = a[left++];
+            else
+                tmp[k++] = a[right++];
+
+        while(left <= leftEnd)    // Copy rest of first half
+            tmp[k++] = a[left++];
+
+        while(right <= rightEnd)  // Copy rest of right half
+            tmp[k++] = a[right++];
+
+        // Copy tmp back
+        for(int i = 0; i < num; i++, rightEnd--)
+            a[rightEnd] = tmp[rightEnd];
+    }
+  
+  public static void main(String[] args)
+  {
+      long inicioMs, finalMs, inicioIs, finalIs, tMs, tIs;
+     
+      for(int i = 0; i < 20; i++){
+      
+      int [] X = CrearArray();
+      int [] Y = CrearArray();
+      
+      int [][] XY = {X,Y}; 
+      inicioMs = System.currentTimeMillis(); 
+      Code_source.mergeSort(X);
+      finalMs = System.currentTimeMillis();
+      
+      inicioIs = System.currentTimeMillis();
+      Code_source.InsertionSort(X);
+      finalIs = System.currentTimeMillis();
+      
+      tMs = (finalMs - inicioMs);
+      
+      tIs = (finalIs - inicioIs);
+      
+      //System.out.println("Tiempo Merge sort : " + tMs + "\n" + "Tiempo Insert sort : " + tIs);
+      System.out.println(X.length * Y.length);
+      System.out.println();
+      System.out.println( tMs + "\n" + tIs);
+      }
+      //System.out.println(Code_source.ArraySum(X) + " " + Code_source.ArrayMax(X) + "\n" );      
+      //}
+  }
+  
 }
