518836ca53833b8ec3c36c07856b9a759ddb08af
Alismos <46933082+Alismos@users.noreply.github.com>
2019-05-05 18:32:54 -0500

    Create sucessors.java

---
+++ laboratorios/lab05/codigo/sucessors.java
@@ -0,0 +1,62 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+import javafx.util.Pair;
+/**
+ * Write a description of class Sucessors here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class sucessors
+{
+    HashMap<String,LinkedList<Pair<String,String>>> struct;
+    
+    public sucessors(){
+       struct = new HashMap<String, LinkedList<Pair<String,String>>>(335792);
+    }
+
+    public void addS(String source, String weight, String destiny){
+
+        if(struct.containsKey(source)==true)
+          struct.get(source).add(new Pair(weight, destiny));
+
+        else{
+         struct.put(source, new LinkedList<Pair<String, String>>());
+         struct.get(source).add(new Pair(weight,destiny));
+      }
+    }
+
+    public void printS(String source){
+        
+        if(struct.containsKey(source)){
+        //LinkedList<Pair<String, String>> temp = new LinkedList<Pair<String,String>>();
+        System.out.println();
+        for(int i = 0; i < struct.get(source).size(); i++){
+           //temp.add(new Pair(struct.get(source).get(i).getKey(), struct.get(source).get(i).getValue()));
+           System.out.println("ID1 = " + source +"| ID2 = " + struct.get(source).get(i).getValue()  + "| Distancias = " + struct.get(source).get(i).getKey());
+           
+         }
+        }
+        else{
+            System.out.println("hubo un problema");
+        }    
+    }
+
+    public static void main(String[]args){
+        sucessors a = new sucessors();
+        a.addS("Su mas", "x1", "La suya");
+        a.addS("Su ma", "x1", "La suya");
+        a.addS("Su ma", "x3", "La suya");
+        a.addS("Su ma", "x4", "La suya");
+        a.addS("Su mas", "x1", "La suya");
+        a.addS("Su ma", "x2", "La suya");
+        a.addS("Su max", "x1", "La suya");
+        a.printS("Su ma");
+        System.out.println();
+        a.printS("Su mas");
+        System.out.println();
+        a.printS("Su max");
+        
+
+    }
+}
