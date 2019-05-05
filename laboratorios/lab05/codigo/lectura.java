c93965fb5e9fb3c0ca536520cf49dc580d11979d
Alismos <46933082+Alismos@users.noreply.github.com>
2019-05-05 18:31:19 -0500

    Update and rename Lectura_1.java to lectura.java

---
+++ laboratorios/lab05/codigo/lectura.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.File;
+import java.util.Scanner;	
+import java.util.LinkedList;
+import javafx.util.Pair;
+import java.util.HashMap;
+
+public class lectura{
+
+     LinkedList <String> ub;
+    public lectura(){
+
+      ub = new LinkedList<String>();
+    }
+
+    public void imprimir () throws FileNotFoundException, IOException{
+       sucessors a= new sucessors();
+
+       a = lectura();
+        
+       for(int i = 0; i < ub.size(); i++){
+           a.printS(ub.get(i));
+        }
+    }
+
+    public sucessors lectura() throws FileNotFoundException, IOException{
+
+        BufferedReader Lec = new BufferedReader (new FileReader ("D:/Alismos/Documentos/Eafit/Segundo semestre/Estructuras de datos/Laboratorios/Laboratorio 5/medellin_colombia-grande.txt"));
+        sucessors struct = new sucessors();
+
+        String Lec1; Lec.readLine();
+        String temp1[];
+
+        while((Lec1 = Lec.readLine())!=null){
+            temp1= Lec1.split(" ");
+
+           if(temp1[0].equals("Arcos."))
+           	break;
+        }
+
+        while((Lec1 = Lec.readLine())!=null){
+         temp1 = Lec1.split(" ");
+         ub.add(temp1[0]);
+         struct.addS(temp1[0],temp1[2], temp1[1]);
+        } 
+        return struct;
+    }
+
+    public static void main(String[]args)throws FileNotFoundException, IOException{
+       lectura a = new lectura();
+       a.imprimir();
+    }
+}
